b650e8d8d9e9468d7b6abc489aa8e257d21f70bb
Asiegbu-Cynthia-Chisom <asiegbucynthia.cc@gmail.com>
2021-09-05 00:16:02 +0100

    update frontend configurations

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -6,9 +6,9 @@ export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: '', // the auth0 domain prefix
-    audience: '', // the audience set for the auth0 app
-    clientId: '', // the client id generated for the auth0 app
-    callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
+    url: 'coffee-shop-auth.us', // the auth0 domain prefix
+    audience: 'coffee', // the audience set for the auth0 app
+    clientId: 'YuBa9k6Qf6N528cUlsPKQQEM6upqMwXJ', // the client id generated for the auth0 app
+    callbackURL: 'http://127.0.0.1:8100/login-results', // the base url of the running ionic application. 
   }
 };
